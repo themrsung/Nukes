8d2c4aa6c0f50745fa9e27a9d425bb76acd7ff46
MJ Sung <themrsung@gmail.com>
2024-04-13 08:45:36 +0900

    ASD

---
+++ src/main/java/pegasus/nukes/Nukes.java
@@ -0,0 +1,31 @@
+package pegasus.nukes;
+
+import org.bukkit.Bukkit;
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityExplodeEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Nukes extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        Bukkit.getPluginManager().registerEvents(new Listener() {
+            public void onTntGoBoom(EntityExplodeEvent e) {
+                if (e.isCancelled()) return;
+                if (!(e.getEntityType() == EntityType.PRIMED_TNT || e.getEntityType() == EntityType.MINECART_TNT)) return;
+
+                var location = e.getLocation();
+                var world = location.getWorld();
+
+                if (world == null) return;
+
+                var thread = new Thread(() -> {
+                    world.createExplosion(location, 175);
+                });
+
+                thread.start();
+                e.setCancelled(true);
+            }
+        }, this);
+    }
+}
